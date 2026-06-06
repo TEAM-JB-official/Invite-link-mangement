5e7712acbff936040f92dbd1696a998eaff21d32
TEAM-JB-official <teamjb80@gmail.com>
2026-06-06 17:42:07 +0530

    Create __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,11 @@
+from .start import start
+from .create_link import create_link
+from .active_links import active_links
+from .revoke import revoke_link, revoke_all
+from .stats import stats
+from .settings import settings
+from .admins import admins
+from .backup import backup, restore
+from .dashboard import dashboard
+from .join_request import join_request
+from .callback_handlers import callback_handlers
