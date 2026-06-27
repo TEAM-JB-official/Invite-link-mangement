50974e0ee9b57ae13cc08b1e965a08216b61d19d
TEAM-JB-official <teamjb80@gmail.com>
2026-06-27 12:53:50 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "8205160181:AAGK2wBXe_uaR_jqN6iuLhAY63Q5f0gEP
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://rs92573993688:pVf4EeDuRi2o92ex@cluster0.9u29q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "invite_bot")
 OWNER_IDS = [int(x.strip()) for x in os.getenv("OWNER_IDS", "5520093674, 1955406483").split(",") if x.strip()]
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1003974194245"))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1004436822292"))
 PORT = int(os.getenv("PORT", 8000))
 NORMAL_MAX_DAYS = int(os.getenv("NORMAL_MAX_DAYS", 7))
 PREMIUM_MAX_DAYS = int(os.getenv("PREMIUM_MAX_DAYS", 30))
